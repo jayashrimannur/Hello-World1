a95c97b5888f4a89193e34c94b75c406bad1d7c1
jayashri <jmannur@gmail.com>
2025-01-29 15:04:23 +0530

    29Jan new branch

--- src/amazontest/BaseTest.java
+++ src/amazontest/BaseTest.java
@@ -61,6 +61,19 @@ public class BaseTest {
   	   System.out.println("postJira10");
 	}
 
+	@Test
+	public void postDevelop()
+	{
+	   System.out.println("postJira6");
+   	   System.out.println("postJira7");
+	   System.out.println("postJira8");
+           //New Update
+	  //From User2
+		
+	   System.out.println("postJira9");
+  	   System.out.println("postJira10");
+	}
+
 	public void initializeDriver() throws IOException
 	{
 	
